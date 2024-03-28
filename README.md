c161f39a864a504746f167d2ff7c7de8b386bfe8
ermelinda-q <ermelinda.qejvani@gmail.com>
2024-03-28 22:50:29 +0000

    Week 3 update

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Filename: _helloworld.py_
 
 ***
 
-__Week 2__
+__Week 2 - Statements__
 
 Filename: bank.py
 
@@ -29,7 +29,24 @@ References:
 
 ***
 
-__Week 3__
+__Week 3 - Variables__
+
+Filename: accounts.py
+
+Task: Write a program that:
+1. Reads in a 10 character account number
+2. Outputs the account number with only the last 4 digits and the first 6 digits replaced with Xs
+3. Modify the program to deal with account numbers of any length
+
+References:
+1. Week 3 lecture on Variables by Andrew Beatty
+2. [Modify Strings](https://www.w3schools.com/python/python_strings_modify.asp)
+3. [Conditional Statements in Python](https://realpython.com/python-conditional-statements/)
+
+***
+
+__Week 4__
+
 
 
 
--- accounts.py
+++ accounts.py
@@ -1,5 +1,5 @@
 # Programming and Scripting (coding with Python)
-# Weekly Task 03
+# Weekly Task 03 -  Statements
 
 # Program Title: Reading a bank account and displaying only the last 4 digits
 # This program reads in an account number and displays only the last 4 digits
@@ -7,24 +7,19 @@
 
 # Author: Ermelinda Qejvani
 
-# 'account_number' is the account number entered by user - integer format
-account_number = int(input('Enter your account number: '))
+account_number = int(input('Enter your account number: '))                  # variable account_number will hold an integer value from user input
+account_number_string = str(account_number)                                 # convert to string and hold it to account_number_string variable
 
-# reading the 'account_number' as a string
-account_number_string = str(account_number)
+account_number_length = len(account_number_string)                          # getting the length of the string 'account_number_string'
 
-# 'acount_number_length' is a variable to get the length of the number entered through 'account_number_string'
-account_number_length = len(account_number_string)
-
-# account number can be any length so I'm using 'if, else' function to check the length first:
-
-if account_number_length > 4: # checking if account number is greater than 4 digits
-    # creating a variable that will add an 'X' to all digits until the last 4 ones and then will display 
-    # just the four digits of account_number_string(it counts from the last digit: -1, -2, etc)
-    display_account_number = 'X' * (account_number_length -4) + account_number_string[-4:]
-    print('The account number you entered is: ', display_account_number)   # print message + the value of 'display_account_number' value
-else:           # if the 'account_number_length'is 4 or less digits
-      print('The account number you entered is: ',account_number_string )
+if account_number_length > 4:               # checking the length of account: if > 4
+    # multiplying by X a string (part of string till the 4 digit from the end substitutes them all by X)
+    # display_account_number is a variable that holds the new string modified 
+    display_account_number = 'X' * (account_number_length -4) + account_number_string[-4:] 
+    # print message with the account number modified
+    print('The account number you entered is: ', display_account_number) 
+else:           # if account number is less then four digits, print the account number as it is entered by user
+    print('The account number you entered is too short\nThe account number you entered is: ',account_number_string )
 
 
 # The following code is a simple solution if the 'account_number' value was just 10 digits as stated at the start of the task 3.
